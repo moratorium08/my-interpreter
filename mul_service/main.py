4498586b1c64cd82b1538788c0a3190babeaeb33
moratorium08 <moratorium08@gmail.com>
2018-02-27 01:26:14 +0900

    Add mul service

---
+++ mul_service/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return jsonify({'status': 'ok'})
+    params = request.json['params']
+    if len(params) > 100:
+        return jsonify({'status': 'fail', 'error': 'too much args'})
+
+    ret = 1
+    for param in params:
+        ret *= param
+    return jsonify({'status': 'ok', 'result': ret})
+
+
+app.run(host='0.0.0.0')
